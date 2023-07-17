9d81ef9399ca69fb717bf2d5aae3bed4d47b45bd
tiago369 <tiagobarreto581@gmail.com>
2023-07-17 13:12:27 -0300

    Add lint and doxygen

--- include/nerian_stereo/camera_info_splitter.hpp
+++ include/nerian_stereo/camera_info_splitter.hpp
@@ -1,3 +1,19 @@
+// Copyright (c) 2023, SENAI Cimatec
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+#ifndef NERIAN_STEREO__CAMERA_INFO_SPLITTER_HPP_
+#define NERIAN_STEREO__CAMERA_INFO_SPLITTER_HPP_
+
 #define BOOST_BIND_NO_PLACEHOLDERS
 #include <chrono>
 #include <functional>
@@ -8,21 +24,28 @@
 #include "nerian_stereo/nerian_stereo_node.h"
 #include "sensor_msgs/msg/camera_info.h"
 
-using namespace std::chrono_literals;
-using std::placeholders::_1;
-
 class CameraInfoSplitter : public rclcpp::Node
 {
     public:
+    /**
+     * @brief Construct a new Camera Info Splitter object
+     * 
+     */
         CameraInfoSplitter();
-    
+
     private:
-        //Declaring the subscriber variable
         rclcpp::Subscription<nerian_stereo::msg::StereoCameraInfo>::SharedPtr nerian_camera_info_;
 
-        //Declare the publishers for the camera left and right
+        // Declare the publishers for the camera left and right
         rclcpp::Publisher<sensor_msgs::msg::CameraInfo>::SharedPtr camera_left_info_;
         rclcpp::Publisher<sensor_msgs::msg::CameraInfo>::SharedPtr camera_right_info_;
 
+        /**
+         * @brief Create a callback for nerian stereo_camera_info
+         * 
+         * @param msg stereo_camera_info message
+         */
         void nerian_info_callback(const nerian_stereo::msg::StereoCameraInfo::SharedPtr msg) const;
 };
+
+#endif  // NERIAN_STEREO__CAMERA_INFO_SPLITTER_HPP_
